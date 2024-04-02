530f26bc5b7b016df925e693b0f889acb02080d9
Peter <paimasiko3@gatech.edu>
2024-04-02 18:50:12 -0400

    added new frontend

--- src/pages/trainingDashboard.js
+++ src/pages/trainingDashboard.js
@@ -1,10 +1,33 @@
-import Sidebar from "@/components/Sidebar";
+import Sidebar from "@/components/sidebar";
+
 
 
 
 
 export default function trainingDashboard() {
     return (
-        <Sidebar/>
+        <main class="overflow-hidden">
+            <nav class="bg-white border-gray-300 shadow-md shadow-red-500/40">
+                <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                    <a class="flex items-center space-x-3 rtl:space-x-reverse">
+                        <img src="images/appLogo.png" class="h-8"/>
+                        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Progress</span>
+                    </a>
+                    <a class="flex items-center space-x-3 rtl:space-x-reverse">
+                    <input type="text" id="simple-search" class="bg-gray-50 w-full border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-2.5 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required />
+                        <button type="submit" class="p-2.5 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                            </svg>
+                            <span class="sr-only">Search</span>
+                        </button>
+                    </a> 
+
+                </div>
+            </nav>
+            <Sidebar/>
+            <div class="h-16 border-l border-gray-400"></div>
+        </main>
+    
     );
 }
