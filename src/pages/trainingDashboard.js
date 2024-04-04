4baffb74e07935d12c9716b6d523658030137759
Ivan Wang <ivanwang1239@gmail.com>
2024-04-04 17:59:40 -0400

    implement animals dashboard

--- src/pages/trainingDashboard.js
+++ src/pages/trainingDashboard.js
@@ -1,8 +1,5 @@
 import Sidebar from "@/components/sidebar";
-
-
-
-
+import TrainingLogCard from "@/components/trainingLogCard";
 
 export default function trainingDashboard() {
     return (
@@ -46,46 +43,10 @@ export default function trainingDashboard() {
                     </div>
                     <hr className="bg-gray-300 w-full h-[2px]"></hr>
                     <div className="flex flex-col justify-start items-center w-10/12 h-screen ml-[100px] mt-[20px]">
-                        <div className="flex flex-row justify-start items-start bg-white w-full h-[110px] mt-[10px] mb-[10px] rounded-xl drop-shadow-xl">
-                            <div className="flex flex-col justify-center items-center bg-blue-900 h-full w-[110px] rounded-l-xl">
-                                <p className="font-extrabold text-4xl text-white">
-                                    20
-                                </p>
-                                <p className="font-normal text-white">
-                                    Oct - 2023
-                                </p>
-                            </div>
-                            <div className="flex flex-col w-4/5 h-full">
-                                <div class="flex flex-row ml-[20px] mt-[5px]">
-                                    <p className="font-bold text-2xl">
-                                        Complete sit lessons
-                                    </p>
-                                    <p className="text-gray-400 align-bottom pt-[6px] ml-[5px]">
-                                        - 20 hours
-                                    </p>
-                                </div>
-                                <div class="flex flex-row ml-[20px] mb-[5px]">
-                                    <p className="text-gray-400 align-bottom">
-                                        Long Lam - Golden Retriever - Lucy
-                                    </p>
-                                </div>
-                                <div class="flex flex-row ml-[20px] mt-[5px]">
-                                    <p className="align-bottom">
-                                        Lucy finishes the sit lessons very well today. Should give her a treat
-                                    </p>
-                                </div>
-                            </div>
-                            <div className="flex flex-col justify-center items-center w-[100px] h-full rounded-r-xl">
-                                <div className="flex justify-center items-center h-14 w-14 bg-red-500 pl-[5px] pr-[5px] pt-[5px] pb-[5px] rounded-[30px]">
-                                    <img src="images/activeTrainingLogo.png" class="h-7"></img>
-                                </div>
-                            </div>
-                        </div>
-                        
+                        <TrainingLogCard/>
                     </div>
                 </div>
             </div>
         </main>
-    
     );
 }
