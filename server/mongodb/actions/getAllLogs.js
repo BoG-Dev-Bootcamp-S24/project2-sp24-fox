a4bcbe6c2615686c3c9ae0eb271926875b19f309
Peter <paimasiko3@gatech.edu>
2024-04-10 21:13:16 -0400

    training admin page complete

--- server/mongodb/actions/getAllLogs.js
+++ server/mongodb/actions/getAllLogs.js
@@ -1,6 +1,25 @@
+import connectDB from ".."
+import Animal from "../models/Animal";
+import TrainingLog from "../models/TrainingLog";
+import User from "../models/User";
+
 export default async function getAllLogs() {
     try {
-        
+        await connectDB()
+
+        const result = await TrainingLog.find({}).lean()
+
+        const newResult = await Promise.all(result.map(async(element) => {
+            const name = await User.findById({_id: element.user})
+            const animal = await Animal.findById({_id: element.animal})
+            element.user = name.fullName;
+            element.animal = animal.name;
+            element.breed = animal.breed;
+            
+            return element
+        }));
+        return Promise.all(newResult)
+
     } catch (error) {
         console.error(error)
     }
